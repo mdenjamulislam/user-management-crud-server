005b44fd268a23ed5ab1f97c49b97bdd5a2510ac
MD Enjamul Islam <mdenjamulislam527@gmail.com>
2024-05-29 11:30:45 +0600

    Server Configration

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
 // Here is the main entry point of the application.
+const express = require("express");
+const cors = require("cors");
+require('dotenv').config();
+const app = express();
+const port = process.env.PORT || 5000;
+
+app.use(cors());
+app.use(express.json());
+
+
+// Routes
+app.get('/', (req, res) => {
+    res.send('Server is running...');
+});
+
+
+app.listen(port, () => {
+    console.log(`Server is running on port: ${port}`);
+})
