c02684830786f8b7584658d25bba6927e667a0c5
MD Enjamul Islam <mdenjamulislam527@gmail.com>
2024-05-29 11:51:33 +0600

    Database connected

--- index.js
+++ index.js
@@ -9,6 +9,41 @@ app.use(cors());
 app.use(express.json());
 
 
+// MongoDB Connection
+
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.yuxa2nd.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+    serverApi: {
+        version: ServerApiVersion.v1,
+        strict: true,
+        deprecationErrors: true,
+    }
+});
+
+async function run() {
+    try {
+        // Connect the client to the server	(optional starting in v4.7)
+        await client.connect();
+        // Send a ping to confirm a successful connection
+        const userCollection = client.db('usersData').collection('users');
+
+        app.post('/users', async (req, res) => {
+            const newUser = req.body;
+            console.log('adding new user: ', newUser);
+        })
+
+    } finally {
+        // Ensures that the client will close when you finish/error
+        // await client.close();
+    }
+}
+run().catch(console.dir);
+
+
+
 // Routes
 app.get('/', (req, res) => {
     res.send('Server is running...');
